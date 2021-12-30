c831bb5033a47c9915eaeacad4f70421093dd7c8
CrazzysMario <87815917+CrazzysMario@users.noreply.github.com>
2021-12-30 23:08:49 +0700

    Add files via upload

---
+++ page.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".control nav a").click(function (event) {
+    event.preventDefault();
+    history.pushState(null, null, event.target.href);
+    $("article").load(event.target.href + " article>.content");
+  });
+  $(window).on("popstate", function (event) {
+    $("article").load(event.target.href + " article>.content");
+  });
+});
